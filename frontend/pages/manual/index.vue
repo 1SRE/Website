1fb8fafc689ee28f94e00240dc366fa005f9a153
Nishant Srivastava <nishant@rsrivastava.io>
2023-04-03 23:21:39 -0400

    update

--- frontend/pages/manual/index.vue
+++ frontend/pages/manual/index.vue
@@ -2,50 +2,107 @@
   <v-container>
     <!-- <h1 v-for="(value, key) in data" :key="(value, key)">{{ key }}</h1> -->
     <div v-for="(value, key) in data" :key="(value, key)">
-      <h1>{{ key }}</h1>
+      <h1>{{ value.title }}</h1>
       <v-row justify="center" align="center">
         <v-col align-self="stretch" cols="auto" sm="3" md="3">
-          <v-card height="100%">
-            <v-card-title>{{ cardTitle(value)[0] }}</v-card-title>
+          <v-card
+            @click="route(value.title, value.content[0])"
+            height="100%"
+            min-width="100%"
+            max-width="100%"
+          >
+            <v-card-title>{{ value.content[0].title }}</v-card-title>
             <v-card-text>
-              <p>
-                {{ cardText(value)[0] }}
-              </p>
+              <p>{{ shorten(value.content[0].text) }}</p>
             </v-card-text>
           </v-card>
         </v-col>
-        <v-col align-self="stretch" cols="auto" sm="3" md="3">
-          <v-card height="100%">
-            <v-card-title>{{ cardTitle(value)[1] }}</v-card-title>
+        <v-col
+          v-if="value.content.length >= 2"
+          align-self="stretch"
+          cols="auto"
+          sm="3"
+          md="3"
+        >
+          <v-card
+            @click="route(value.title, value.content[1])"
+            height="100%"
+            min-width="100%"
+            max-width="100%"
+          >
+            <v-card-title>{{ value.content[1].title }}</v-card-title>
             <v-card-text>
-              <p>
-                {{ cardText(value)[1] }}
-              </p>
+              <p>{{ shorten(value.content[1].text) }}</p>
             </v-card-text>
           </v-card>
         </v-col>
-        <v-col align-self="stretch" cols="auto" sm="3" md="3">
-          <v-card height="100%">
-            <v-card-title>{{ cardTitle(value)[2] }}</v-card-title>
+        <v-col v-else>
+          <v-skeleton-loader
+            type="card"
+            height="100%"
+            min-width="100%"
+            max-width="100%"
+          ></v-skeleton-loader>
+        </v-col>
+        <v-col
+          v-if="value.content.length >= 3"
+          align-self="stretch"
+          cols="auto"
+          sm="3"
+          md="3"
+        >
+          <v-card
+            @click="route(value.title, value.content[2])"
+            height="100%"
+            min-width="100%"
+            max-width="100%"
+          >
+            <v-card-title>asdfasdfasdf {{ value.content[2].title }}</v-card-title>
             <v-card-text>
-              <p>
-                {{ cardText(value)[2] }}
-              </p>
+              <p>{{ shorten(value.content[2].text) }}</p>
             </v-card-text>
           </v-card>
         </v-col>
-        <v-col align-self="stretch" cols="auto" sm="3" md="3">
-          <v-card height="100%">
-            <v-card-title>{{ cardTitle(value)[3] }}</v-card-title>
+        <v-col v-else>
+          <v-skeleton-loader
+            type="card"
+            height="100%"
+            min-width="100%"
+            max-width="100%"
+          ></v-skeleton-loader>
+        </v-col>
+        <v-col
+          v-if="value.content.length >= 4"
+          align-self="stretch"
+          cols="auto"
+          sm="3"
+          md="3"
+        >
+          <v-card
+            @click="route(value.title, value.content[3])"
+            height="100%"
+            min-width="100%"
+            max-width="100%"
+          >
+            <v-card-title>{{ value.content[3].title }}</v-card-title>
             <v-card-text>
-              <p>
-                {{ cardText(value)[3] }}
-              </p>
+              <p>{{ shorten(value.content[3].text) }}</p>
             </v-card-text>
           </v-card>
         </v-col>
-        <p><a :href="preText+key">{{ key }}</a></p>
+        <v-col v-else>
+          <v-skeleton-loader
+            type="card"
+            height="100%"
+            min-width="100%"
+            max-width="100%"
+          ></v-skeleton-loader>
+        </v-col>
       </v-row>
+      <center><p>
+        <a :href="preText + key">click for more</a>
+      </p></center>
+      
     </div>
   </v-container>
 </template>
@@ -59,26 +116,27 @@ export default {
   data() {
     return {
       data: data,
-      preText: "/manual/"
+      preText: "/manual/",
     };
   },
   methods: {
-    cardTitle(innerData) {
-      var titles = [];
-      for (var index = 0; index < Object.keys(innerData).length; index++) {
-        titles.push(Object.keys(innerData)[index].split(".md")[0]);
-      }
-      return titles;
+    shorten(txt) {
+      // console.log(txt)
+      if (txt === undefined) return "This is a folder";
+      else
+        return (
+          marked
+            .parse(txt)
+            .replace(/<[^>]*>/g, "")
+            .replace("&#39;", "'")
+            .split(/\s+/)
+            .slice(0, 50)
+            .join(" ") + "..."
+        );
     },
-    cardText(innerData) {
-        console.log(innerData)
-        var text = []
-        for (var index = 0; index < Object.values(innerData).length; index++) {
-            if (typeof Object.values(innerData)[index] === 'object') text.push("Click to visit the contents of the folder")
-            else text.push(marked.parse(Object.values(innerData)[index]).replace(/<[^>]*>/g, '').replace("&#39;", "'").split(/\s+/).slice(0, 50).join(" ")+"...")
-            // console.log(marked.parse(Object.values(innerData)[index]))
-        }
-      return text;
+    route(key, value) {
+      if (value.text === undefined) return window.location.href = "/manual/" + key;
+      else return window.location.href = "/manual/" + key + "/" + value.title;
     },
   },
   mounted() {},
