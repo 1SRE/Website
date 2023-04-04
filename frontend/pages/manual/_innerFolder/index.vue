d9809b52a7cc902d005b49ccb76fa0587fb88423
Nishant Srivastava <nishant@rsrivastava.io>
2023-04-03 23:43:15 -0400

    update

--- frontend/pages/manual/_innerFolder/index.vue
+++ frontend/pages/manual/_innerFolder/index.vue
@@ -1,109 +1,18 @@
 <template>
   <v-container>
     <!-- <h1 v-for="(value, key) in data" :key="(value, key)">{{ key }}</h1> -->
-    <div v-for="(value, key) in data" :key="(value, key)">
-      <h1>{{ value.title }}</h1>
-      <v-row justify="center" align="center">
-        <v-col align-self="stretch" cols="auto" sm="3" md="3">
-          <v-card
-            @click="route(value.title, value.content[0])"
-            height="100%"
-            min-width="100%"
-            max-width="100%"
-          >
-            <v-card-title>{{ value.content[0].title }}</v-card-title>
-            <v-card-text>
-              <p>{{ shorten(value.content[0].text) }}</p>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col
-          v-if="value.content.length >= 2"
-          align-self="stretch"
-          cols="auto"
-          sm="3"
-          md="3"
-        >
-          <v-card
-            @click="route(value.title, value.content[1])"
-            height="100%"
-            min-width="100%"
-            max-width="100%"
-          >
-            <v-card-title>{{ value.content[1].title }}</v-card-title>
-            <v-card-text>
-              <p>{{ shorten(value.content[1].text) }}</p>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col v-else>
-          <v-skeleton-loader
-            type="card"
-            height="100%"
-            min-width="100%"
-            max-width="100%"
-          ></v-skeleton-loader>
-        </v-col>
-        <v-col
-          v-if="value.content.length >= 3"
-          align-self="stretch"
-          cols="auto"
-          sm="3"
-          md="3"
-        >
-          <v-card
-            @click="route(value.title, value.content[2])"
-            height="100%"
-            min-width="100%"
-            max-width="100%"
-          >
-            <v-card-title>{{ value.content[2].title }}</v-card-title>
-            <v-card-text>
-              <p>{{ shorten(value.content[2].text) }}</p>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col v-else>
-          <v-skeleton-loader
-            type="card"
-            height="100%"
-            min-width="100%"
-            max-width="100%"
-          ></v-skeleton-loader>
-        </v-col>
-        <v-col
-          v-if="value.content.length >= 4"
-          align-self="stretch"
-          cols="auto"
-          sm="3"
-          md="3"
-        >
-          <v-card
-            @click="route(value.title, value.content[3])"
-            height="100%"
-            min-width="100%"
-            max-width="100%"
-          >
-            <v-card-title>{{ value.content[3].title }}</v-card-title>
-            <v-card-text>
-              <p>{{ shorten(value.content[3].text) }}</p>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col v-else>
-          <v-skeleton-loader
-            type="card"
-            height="100%"
-            min-width="100%"
-            max-width="100%"
-          ></v-skeleton-loader>
-        </v-col>
-      </v-row>
-      <center><p>
-        <a :href="preText + value.title">click for more</a>
-      </p></center>
-      
-    </div>
+
+    <h1>{{ folderName }}</h1>
+    <v-row justify="left" align="center">
+      <v-col align-self="stretch" cols="auto" sm="3" md="3">
+        <v-card height="100%" min-width="100%" max-width="100%">
+          <v-card-title>asdf</v-card-title>
+          <v-card-text>
+            <p>{{ shorten("alksdjfasdf") }}</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -116,8 +25,11 @@ export default {
   data() {
     return {
       data: data,
-      preText: "/manual/",
     };
+  },
+  async asyncData({ params }) {
+    const folderName = params.innerFolder;
+    return { folderName };
   },
   methods: {
     shorten(txt) {
@@ -135,8 +47,9 @@ export default {
         );
     },
     route(key, value) {
-      if (value.text === undefined) return window.location.href = "/manual/" + key;
-      else return window.location.href = "/manual/" + key + "/" + value.title;
+      if (value.text === undefined)
+        return (window.location.href = "/manual/" + key);
+      else return (window.location.href = "/manual/" + key + "/" + value.title);
     },
   },
   mounted() {},
