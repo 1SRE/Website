e24abd95bb8c57dcfb896dcf68ef261826f83193
Nishant Srivastava <nishant@rsrivastava.io>
2023-04-13 00:58:12 -0400

    login/logout

--- pages/register.vue
+++ pages/register.vue
@@ -6,10 +6,17 @@
         <v-flex xs16 sm12 md8>
           <v-card class="elevation-12">
             <v-toolbar class="text-center" dark>
-              <v-toolbar-title>Login</v-toolbar-title>
+              <v-toolbar-title>Register</v-toolbar-title>
             </v-toolbar>
             <v-card-text>
               <v-form>
+                <v-text-field
+                  prepend-icon="mdi-email"
+                  name="email"
+                  label="email"
+                  type="text"
+                  v-model="userInfo.email"
+                ></v-text-field>
                 <v-text-field
                   prepend-icon="mdi-account-circle"
                   name="username"
@@ -22,13 +29,21 @@
                   name="password"
                   label="Password"
                   type="password"
-                  v-model="userInfo.password"
+                  v-model="userInfo.password1"
+                ></v-text-field>
+                <v-text-field
+                  prepend-icon="mdi-lock"
+                  name="password"
+                  label="Password"
+                  type="password"
+                  v-model="userInfo.password2"
                 ></v-text-field>
               </v-form>
             </v-card-text>
-            <v-card-subtitle><center><a id="createAccountTXT" href="/register">Don't have an account?</a></center></v-card-subtitle>
+            <v-card-subtitle><center><a id="loginAccountTXT" href="/login">Already signed up?</a></center></v-card-subtitle>
+
             <v-card-actions>
-              <v-btn block color="success" v-on:click="loginUser(userInfo)">Login</v-btn>
+              <v-btn block color="error" v-on:click="registerUser(userInfo)">Register</v-btn>
             </v-card-actions>
           </v-card>
         </v-flex>
@@ -37,25 +52,23 @@
   </v-container>
 </template>
 
-<style scoped>
-#createAccountTXT {
-  color: #ed5555;
-}
-</style>
+
 
 <script>
 export default {
-  name: "Login",
+  name: "Register",
   data() {
     return {
       userInfo: {
         username: "nishipoo",
-        password: "asdfj@!(jfK!@",
+        email: "someEmail@gmail.com",
+        password1: "asdfj@!(jfK!@",
+        password2: "asdfj@!(jfK!@",
       },
     };
   },
   methods: {
-    async loginUser(userInfo) {
+    async registerUser(userInfo) {
         try {
           let response = await this.$auth.loginWith('local', {data: this.userInfo})
           if (response["status"] === 200) this.$auth.setUser("asdasd")
@@ -69,4 +82,6 @@ export default {
 </script>
 
 <style>
+#loginAccountTXT {
+}
 </style>
